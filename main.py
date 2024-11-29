bdb288b8f073d37a2dd27a3c9f314675d91db50e
yotam-biu <nagaryo3@biu.ac.il>
2024-11-29 21:00:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from string_utils import parse_reaction, count_atoms_for_each_mol
-# from equations_utils import build_equations, solve
+from equations_utils import build_equations, solve
 
 
 
@@ -9,20 +9,20 @@ from sympy import symbols, Eq
 from sympy import solve as sympy_solve
 
 
-ELEMENTS = [
-    'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
-    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
-    'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
-    'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr',
-    'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn',
-    'Sb', 'I', 'Te', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
-    'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
-    'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
-    'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
-    'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
-    'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
-    'Rg', 'Cn', 'Uut', 'Uuq', 'Uup', 'Uuh', 'Uus', 'Uuo'
-]
+# ELEMENTS = [
+#     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+#     'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
+#     'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+#     'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr',
+#     'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn',
+#     'Sb', 'I', 'Te', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
+#     'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
+#     'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+#     'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
+#     'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
+#     'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
+#     'Rg', 'Cn', 'Uut', 'Uuq', 'Uup', 'Uuh', 'Uus', 'Uuo'
+# ]
 
 
 
@@ -30,40 +30,40 @@ ELEMENTS = [
 
 
 
-def generate_equation_for_element(compounds, coefficients, element):
-    equation = 0
-    for i, compound in enumerate(compounds):
-        if element in compound:
-            equation += coefficients[i] * compound[element]
-    return equation
+# def generate_equation_for_element(compounds, coefficients, element):
+#     equation = 0
+#     for i, compound in enumerate(compounds):
+#         if element in compound:
+#             equation += coefficients[i] * compound[element]
+#     return equation
 
 
 
-def build_equations(reactant_atoms, product_atoms):
-    ## coefficients ##
-    reactant_coefficients = list(symbols(f'a0:{len(reactant_atoms)}'))
-    product_coefficients = list(symbols(f'b0:{len(product_atoms)}'))  # Ensure the last coefficient is 1
-    product_coefficients = product_coefficients[:-1] + [1]
+# def build_equations(reactant_atoms, product_atoms):
+#     ## coefficients ##
+#     reactant_coefficients = list(symbols(f'a0:{len(reactant_atoms)}'))
+#     product_coefficients = list(symbols(f'b0:{len(product_atoms)}'))  # Ensure the last coefficient is 1
+#     product_coefficients = product_coefficients[:-1] + [1]
 
-    ## equations ##
-    equations = []
-    for element in ELEMENTS:
-        lhs = generate_equation_for_element(reactant_atoms, reactant_coefficients, element)
-        rhs = generate_equation_for_element(product_atoms, product_coefficients, element)
-        if lhs != 0 or rhs != 0:
-            equations.append(Eq(lhs, rhs))
+#     ## equations ##
+#     equations = []
+#     for element in ELEMENTS:
+#         lhs = generate_equation_for_element(reactant_atoms, reactant_coefficients, element)
+#         rhs = generate_equation_for_element(product_atoms, product_coefficients, element)
+#         if lhs != 0 or rhs != 0:
+#             equations.append(Eq(lhs, rhs))
 
-    return equations, reactant_coefficients + product_coefficients[:-1]
+#     return equations, reactant_coefficients + product_coefficients[:-1]
 
 
-def solve(equations, coefficients):
-    solution = sympy_solve(equations, coefficients)
+# def solve(equations, coefficients):
+#     solution = sympy_solve(equations, coefficients)
 
-    if len(solution) == len(coefficients):
-        coefficient_values = list()
-        for coefficient in coefficients:
-            coefficient_values.append(float(solution[coefficient]))
-        return coefficient_values
+#     if len(solution) == len(coefficients):
+#         coefficient_values = list()
+#         for coefficient in coefficients:
+#             coefficient_values.append(float(solution[coefficient]))
+#         return coefficient_values
 
 
 
