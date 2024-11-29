4341144652300d68e7a188f6409ea6ac70785d3e
yotam-biu <nagaryo3@biu.ac.il>
2024-11-29 21:27:06 +0200

    Update balance_test.py

--- .tests/balance_test.py
+++ .tests/balance_test.py
@@ -1,9 +1,9 @@
-# from main import balance_reaction
-import numpy
+from main import balance_reaction
+# import numpy
 
 def test_balance():
     assert True
-    # assert balance_reaction("Fe2O3 + H2 -> Fe + H2O") == [1/3, 1, 2/3, 1]
+    assert balance_reaction("Fe2O3 + H2 -> Fe + H2O") == [1/3, 1, 2/3, 1]
     # assert balance_reaction("CH4 + O2 -> CO2 + H2O") == [1/2, 1, 1/2, 1]
     # assert balance_reaction("C3H8 + O2 -> CO2 + H2O") == [1/4, 5/4, 3/4, 1]
     # assert balance_reaction("H2 + O2 -> H2O") == [1, 1/2, 1]
