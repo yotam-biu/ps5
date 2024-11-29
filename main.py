420a410cb0498c8902ed4d562f18dc6ef4b923c9
yotam-biu <nagaryo3@biu.ac.il>
2024-11-29 22:51:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from string_utils import parse_chemical_reaction, count_atoms_in_reaction
-from equation_utils import build_equations, my_solve
-
-
-
+# Add the import statements for necessary sympy functions here
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
