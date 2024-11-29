ecbba3f7ae861abc24052dd8b74fc912078bac4b
yotam-biu <nagaryo3@biu.ac.il>
2024-11-29 20:54:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from string_utils import parse_reaction, count_atoms_for_each_mol
-from equations_utils import build_equations, solve
+# from string_utils import parse_reaction, count_atoms_for_each_mol
+# from equations_utils import build_equations, solve
 
-def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
+# def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
-    # 1.parse reaction
-    reactants, products = parse_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = count_atoms_for_each_mol(reactants) # [{"Fe":2, "O":1}, {"H":2}]
-    product_atoms = count_atoms_for_each_mol(products)
+#     # 1.parse reaction
+#     reactants, products = parse_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
+#     reactant_atoms = count_atoms_for_each_mol(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+#     product_atoms = count_atoms_for_each_mol(products)
 
-    # 2.build equation and solve
-    equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    coefficients = solve(equations, coefficients) + [1]
+#     # 2.build equation and solve
+#     equations, coefficients = build_equations(reactant_atoms, product_atoms)
+#     coefficients = solve(equations, coefficients) + [1]
 
-    return coefficients # [1/3, 1, 2/3, 1]
+#     return coefficients # [1/3, 1, 2/3, 1]
+
+
+import numpy
+from sympy import symbols, Eq, solve
+
+
+
+def my_func(a):
+    arr = numpy.array([1])
+    x = symbols('x')
+    solution = solve(Eq(x**2 - 4, 0), x)
+    return a+1
